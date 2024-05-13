535b6b434f5dd39424394f2a9995b2d84afed0a5
Shi Lei <shi_lei@massclouds.com>
2024-05-13 06:39:50 +0000

    loongarch64: support it

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -10,6 +10,9 @@ cfg_if::cfg_if! {
     } else if #[cfg(all(target_arch = "aarch64", platform_family = "aarch64-qemu-virt"))] {
         mod aarch64_qemu_virt;
         pub use self::aarch64_qemu_virt::*;
+    } else if #[cfg(all(target_arch = "loongarch64", platform_family = "loongarch64-qemu-virt"))] {
+        mod loongarch64_qemu_virt;
+        pub use self::loongarch64_qemu_virt::*;
     } else {
         mod dummy;
         pub use self::dummy::*;
