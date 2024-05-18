ff8e59c8e40135d4ac7edd7fdc7a8f8af6a0050f
Shi Lei <shi_lei@massclouds.com>
2024-05-18 06:04:41 +0000

    terminate

--- rt_early_console/src/lib.rs
+++ rt_early_console/src/lib.rs
@@ -6,6 +6,7 @@ use core::panic::PanicInfo;
 pub extern "Rust" fn runtime_main(_cpu_id: usize, _dtb_pa: usize) {
     let msg = "\n[early_console]: Hello, ArceOS!\n";
     early_console::write_bytes(msg.as_bytes());
+    axhal::misc::terminate();
 }
 
 pub fn panic(info: &PanicInfo) -> ! {
